d706091d4b09858329b5c43afd9bcbc273d1d51d
hequan2017 <hequan2011@sina.com>
2026-02-25 11:13:29 +0800

    Update bottomInfo component to reflect branding change from 'flipped-aurorae' to 'kapigpu'; remove unused About page content and streamline script setup.

--- web/src/components/bottomInfo/bottomInfo.vue
+++ web/src/components/bottomInfo/bottomInfo.vue
@@ -32,7 +32,7 @@
   })
 
   console.log(
-    `%c powered by %c flipped-aurorae %c`,
+    `%c powered by %c kapigpu %c`,
     'background:#0081ff; padding: 1px; border-radius: 3px 0 0 3px; color: #fff',
     'background:#354855; padding: 1px 5px; border-radius: 0 3px 3px 0; color: #fff; font-weight: bold;',
     'background:transparent'
